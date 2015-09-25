4eb74e443dd562118177dda640d235887ac87968
Rodrigo Delgado <rdalcega@gmail.com>
2015-09-25 13:19:04 -0700

    Server serves stored levels for requests to /levels/:number

--- paths.js
+++ paths.js
@@ -2,6 +2,12 @@ module.exports = {
 
   index: __dirname + '/client/index.html',
 
-  client: __dirname + '/client'
+  client: __dirname + '/client',
+
+  samples: __dirname + '/server/samples',
+
+  routers: __dirname + '/server/routers',
+
+  models: __dirname + '/server/models'
 
 };
--- server/server.js
+++ server/server.js
@@ -1,15 +1,24 @@
 var express = require('express');
-// var mongoose = require('mongoose');
+
+var mongoose = require('mongoose');
+
 // var morgan = require('morgan');
-// var bodyParser = require('body-parser');
-var app = express();
+
+var bodyParser = require('body-parser');
+
 var paths = require('../paths.js');
-// mongoose.connect('mongodb://localhost/parrot');
+
+var Sequencer = require(paths.models + '/sequencerModel.js');
+
+mongoose.connect('mongodb://localhost/parrot');
+
+var app = express();
 
 // app.use(morgan('dev'));
-// app.use(bodyParser.urlencoded({extended: true}));
-// app.use(bodyParser.json());
-console.log(__dirname);
+
+app.use(bodyParser.urlencoded({extended: true}));
+
+app.use(bodyParser.json());
 
 app.get('/', function(req, res) {
 
@@ -18,17 +27,40 @@ app.get('/', function(req, res) {
 });
 
 app.use(express.static(paths.client));
-app.use('/samples', express.static(__dirname + '/samples'));
 
-var sequencerRouter = express.Router();
+app.use('/samples', express.static(paths.samples));
 
-app.use(function (req, res, next) {
-  console.log('Time:', Date.now());
-  next();
+app.use( '/levels', function( request, response, next ) {
+
+  if( request.method === 'GET' ) {
+
+    var level = +request.url.split('/')[1];
+
+    Sequencer.findOne({'level': level}, 'data', function(err, sequencer) {
+      if (err) { response.send(500, err); }
+      else if (!sequencer) { response.send(404); }
+      else response.send(sequencer.data);
+    });
+
+  }
+
 });
 
-app.use('/', sequencerRouter);
-require('./routers/sequencerRouter.js')(sequencerRouter);
+app.post( '/levels', function( request, response, next ) {
+
+  var sequencer = new Sequencer(); // create a document
+  sequencer.level = request.body.level;
+  sequencer.data = request.body.data;
+
+  // callback function happenes after saving data
+  sequencer.save(function(err){
+    if (err) { response.send(500, err); }
+    else response.send(201);
+  });
+
+});
+
+
 
 app.listen(44100);
 
